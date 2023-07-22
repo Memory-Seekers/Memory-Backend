6f495bef09e24d8f33a0760269086fdd85f25f48
Hyojin <snowfluppy@ajou.ac.kr>
2023-07-22 17:33:55 +0900

    chore: LandmarkService 테스트 코드 수정 - 랜드마크 리스트 조회시 landmarkService 사용하도록 수정 - getLandmarkId로 Id 불러와서 조회하도록 수정

--- src/test/java/lookIT/lookITspring/service/LandmarkServiceTest.java
+++ src/test/java/lookIT/lookITspring/service/LandmarkServiceTest.java
@@ -29,6 +29,7 @@ public class LandmarkServiceTest {
     @DisplayName("랜드마크 리스트 조회")
     public void 랜드마크_리스트_조회() throws Exception {
         // Given
+        Integer size = landmarkRepository.findAllLandmarks().size();
 
         Landmark landmark1 = Landmark.builder()
             .landmarkName("테스트용 랜드마크1")
@@ -48,23 +49,19 @@ public class LandmarkServiceTest {
             .landmarkAddress("대한민국 서울 어딘가2")
             .build();
 
-        // When
         landmarkRepository.save(landmark1);
         landmarkRepository.save(landmark2);
 
-        List<AllLandmarkDto> allLandmarks = landmarkService.allLandmarkInfo();
+        // When
+        AllLandmarkDto checkLandmark1 = landmarkService.allLandmarkInfo().get(size);
+        AllLandmarkDto checkLandmark2 = landmarkService.allLandmarkInfo().get(size+1);
 
         // Then
-        assertEquals("Assertion failed: Number of Landmark is wrong", 6, allLandmarks.size()); //n+2개
-
-        Landmark checkLandmark1 = landmarkRepository.findByLandmarkName("테스트용 랜드마크1");
         assertEquals("Assertion failed: Landmark1 Latitude mismatch", 18.1111354, checkLandmark1.getLandLatitude());
         assertEquals("Assertion failed: Landmark1 Longitude mismatch", 11.2222226, checkLandmark1.getLandLongitude());
 
-        Landmark checkLandmark2 = landmarkRepository.findByLandmarkName("테스트용 랜드마크2");
         assertEquals("Assertion failed: Landmark2 Latitude mismatch", 19.2468, checkLandmark2.getLandLatitude());
         assertEquals("Assertion failed: Landmark2 Longitude mismatch", 12.333625915, checkLandmark2.getLandLongitude());
-
 
     }
 
@@ -73,39 +70,23 @@ public class LandmarkServiceTest {
     public void 랜드마크_ID로_상세정보_조회() throws Exception {
         //Given
         Landmark landmark = Landmark.builder()
-            .landmarkName("테스트용 랜드마크3")
-            .landLatitude(18.1354)
+            .landmarkName("테스트용 랜드마크1")
+            .landLatitude(18.1111354)
             .landLongitude(11.2222226)
-            .landInfo("테스트를 위해 만들어진 임시 랜드마크3")
-            .landmarkAddress("대한민국 서울 어딘가3")
-            .frameUrl("https://unknown3_url.com")
+            .landInfo("테스트를 위해 만들어진 임시 랜드마크1")
+            .frameUrl("https://unknown1_url.com")
+            .landmarkAddress("대한민국 서울 어딘가1")
             .build();
 
         //When
         landmarkRepository.save(landmark);
 
         //Then
-        Landmark checkLandmark = landmarkRepository.findByLandmarkName("테스트용 랜드마크3");
-        assertEquals("Assertion failed: Landmark name mismatch", "테스트용 랜드마크3", checkLandmark.getLandmarkName());
-        assertEquals("Assertion failed: Landmark info mismatch", "테스트를 위해 만들어진 임시 랜드마크3", checkLandmark.getLandInfo());
-        assertEquals("Assertion failed: Landmark url mismatch", "https://unknown3_url.com", checkLandmark.getFrameUrl());
-        assertEquals("Assertion failed: Landmark address mismatch", "대한민국 서울 어딘가3", checkLandmark.getLandmarkAddress());
+        ResponseEntity<LandmarkInfoDto> checkLandmark = landmarkService.landmarkInfoWithLandmarkId(landmark.getLandmarkId());
+        assertEquals("Assertion failed: Landmark name mismatch", "테스트용 랜드마크1", checkLandmark.getBody().getLandmarkName());
+        assertEquals("Assertion failed: Landmark info mismatch", "테스트를 위해 만들어진 임시 랜드마크1", checkLandmark.getBody().getLandInfo());
+        assertEquals("Assertion failed: Landmark url mismatch", "https://unknown1_url.com", checkLandmark.getBody().getFrameUrl());
+        assertEquals("Assertion failed: Landmark address mismatch", "대한민국 서울 어딘가1", checkLandmark.getBody().getLandmarkAddress());
     }
-
-
-    @Test
-    @DisplayName("랜드마크_ID로 상세정보 조회 - 존재하지 않는 ID")
-    public void testLandmarkInfoWithInvalidLandmarkId() {
-        // Given
-        landmarkRepository.deleteAll();
-        Long landmarkId = 1L;
-
-        // When
-        ResponseEntity<LandmarkInfoDto> actualLandmark = landmarkService.landmarkInfoWithLandmarkId(landmarkId);
-
-        // Then
-        assertNull(actualLandmark.getBody(), "Assertion failed: Landmark is not empty");
-    }
-
 
 }
